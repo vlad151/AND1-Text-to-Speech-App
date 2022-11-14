6ef01b5cbda4a442aa540ae9a0e7a4acbdbc47ef
vlad151 <lazar.vlad151@gmail.com>
2022-11-14 12:03:18 +0100

    register user firebase

--- app/src/main/java/com/example/speechful/MainActivity.java
+++ app/src/main/java/com/example/speechful/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.speechful.databinding.ActivityMainBinding;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= ActivityMainBinding.inflate(getLayoutInflater());
+        startActivity(new Intent(getApplicationContext(),RegisterUser.class));
         setContentView(binding.getRoot());
 
         //sets the initial fragment
--- app/src/main/java/com/example/speechful/ProfileFragment.java
+++ app/src/main/java/com/example/speechful/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.speechful;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class ProfileFragment extends Fragment {
 
-
+    private Button registerButton;
+    
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -22,6 +26,9 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+
 }
